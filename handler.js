a3dbeaaf915a84356be34c07294d0d940e8241db
Michael Wuergler <wuergler@gmail.com>
2019-04-10 19:35:59 +0200

    support multiple webhooks

--- handler.js
+++ handler.js
@@ -1,41 +1,57 @@
-const fetch = require('fetch-everywhere')
-const messageBuilder = require('./helpers/messageBuilder')
-const noDataTodayMessage = require('./helpers/noDataTodayMessage')
+const fetch = require('fetch-everywhere');
+const messageBuilder = require('./helpers/messageBuilder');
+const noDataTodayMessage = require('./helpers/noDataTodayMessage');
 const devConfig = require('./config/config.dev.json');
-const getWerdinoData = require('.')
+const getWerdinoData = require('.');
 
-const getWebhookAddress = () => {
-  const envAddress = process.env.SLACK_WEBHOOK_ADDRESS;
-  
-  if (envAddress && typeof envAddress === 'string') {
-    return envAddress;
-  }
-  return devConfig.SLACK_WEBHOOK_ADDRESS;
-}
+const getWebhookAddresses = () => {
+	const addresses = [
+		process.env.SLACK_WEBHOOK_ADDRESS,
+		process.env.SLACK_WEBHOOK_ADDRESS2,
+		process.env.SLACK_WEBHOOK_ADDRESS3,
+	].filter(a => typeof a === 'string');
+
+	if (addresses.length === 0) {
+		return [devConfig.SLACK_WEBHOOK_ADDRESS];
+	}
+
+	return addresses;
+};
 
 function runWerdino() {
-  getWerdinoData().then(data => {
-    let blocks;
+	getWerdinoData()
+		.then(async data => {
+			let blocks;
 
-    if (data.error && data.error === 'NO_MENU_DATA_TODAY') {
-      blocks = noDataTodayMessage()
-    } else {
-      blocks = messageBuilder(data)
-    }
-  
-    fetch(getWebhookAddress(), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ blocks })
-      }
-    )
-      .then(response => {
-        console.log(response.status, response.statusText)
-      })
-      .catch(error => console.error('Error with Slack Webhook:', error))
-  }).catch(err => console.log(`Error in getWerdinoData: ${err}`));
+			if (data.error && data.error === 'NO_MENU_DATA_TODAY') {
+				blocks = noDataTodayMessage();
+			} else {
+				blocks = messageBuilder(data);
+			}
+
+			await Promise.all(
+				getWebhookAddresses().map(address => {
+					return new Promise((resolve, reject) => {
+						fetch(address, {
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json',
+							},
+							body: JSON.stringify({ blocks }),
+						})
+							.then(response => {
+								console.log(response.status, response.statusText);
+								resolve();
+							})
+							.catch(error => {
+								console.log('Error with Slack Webhook:', error);
+								reject();
+							});
+					});
+				})
+			);
+		})
+		.catch(err => console.log(`Error in getWerdinoData: ${err}`));
 }
 
 module.exports.runWerdino = runWerdino;
