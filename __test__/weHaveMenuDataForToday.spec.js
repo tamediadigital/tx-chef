54641cece0061dc5ff473ea7f5f999729fca1688
Michael Wuergler <wuergler@gmail.com>
2019-12-13 23:57:33 +0100

    cleanup the way we check for valid data

--- __test__/weHaveMenuDataForToday.spec.js
+++ __test__/weHaveMenuDataForToday.spec.js
@@ -1,58 +1,25 @@
 const fn = require('../helpers/weHaveMenuDataForToday');
 
-const fixture = {
-  "date": "04.07.19",
-  "meals": {
-    "Brasserie": {
-      "title": "Ceasar Salat mit Speckwürfeli",
-      "description": "(Schweiz) garniert mit Chicken Nuggets (Schweiz) und Croutons",
-      "prices": [
-        "CHF 9,50",
-        "CHF 13,50"
-      ]
-    },
-    "Grün und natürlich": {
-      "title": "Griechische Gemüsepfanne",
-      "description": "mit Feta, Ofenkartoffel mit Tzatzikisauce und Rotkabissalat",
-      "prices": [
-        "CHF 9,50",
-        "CHF 13,50"
-      ]
-    },
-    "Feuer und Flamme": {
-      "title": "Grillierte Jacobsmuscheln (USA)",
-      "description": "nach Grenobler Art mit warmem Antipastigemüse und Pommes Frites",
-      "prices": [
-        "CHF 22,00",
-        "CHF 26,00"
-      ]
-    },
-    "Tagessuppe": {
-      "title": "Fischsuppe a la Bouillabaisse",
-      "description": null,
-      "prices": [
-        "CHF 1,40",
-        "CHF 1,70"
-      ]
-    },
-    "Buddha Bowl": {
-      "title": "Fresh California Bowl",
-      "description": null,
-      "prices": [
-        "CHF 12,50",
-        "CHF 16,50"
-      ]
-    }
-  }
-};
+describe('weHaveMenuDataForToday', () => {
+	test('should return false for days without menu items', () => {
+		const fixture = {
+			date: '1234',
+			meals: {},
+		};
 
-  test('weHaveMenuDataForToday returns false on a weekend', () => {
-    const sat = '06.07.19';
-    const sun = '07.07.19';
-    expect(fn(fixture, sat)).toBe(false);
-    expect(fn(fixture, sun)).toBe(false);
-  });
+		expect(fn(fixture)).toBe(false);
+	});
 
-  test('weHaveMenuDataForToday returns true for valid days', () => {
-    expect(fn(fixture, '04.07.19')).toBe(true);
-  });
+	test('should return false for days without a valid date', () => {
+		const fixture = {
+			date: '',
+			meals: {},
+		};
+
+		expect(fn(fixture)).toBe(false);
+	});
+
+	test('weHaveMenuDataForToday returns true for valid days', () => {
+		expect(fn({ date: '12-34-56', meals: { foo: {} } })).toBe(true);
+	});
+});
--- helpers/weHaveMenuDataForToday.js
+++ helpers/weHaveMenuDataForToday.js
@@ -1,10 +1,10 @@
 /**
  * Validate that we have menu data for the given date key
  * @param {Object} menuData - the scraped data from the online Werdino menue
- * @param {*} todaysItemKey - today's date key in the form of '2019-03-30'
  */
-function weHaveMenuDataForToday(menuData, todaysItemKey) {
-    return menuData.date === todaysItemKey && Object.keys(menuData.meals).length > 0;
+function weHaveMenuDataForToday(menuData) {
+	const { date, meals } = menuData;
+	return typeof date === 'string' && date.trim().length > 0 && meals && Object.keys(meals).length > 0;
 }
 
-module.exports = weHaveMenuDataForToday; 
+module.exports = weHaveMenuDataForToday;
