8a2c44c347287086ca8bcb2cb2fe494db29a66e7
Michael Wuergler <wuergler@gmail.com>
2019-12-21 13:17:46 +0100

    add upload-termonologies.js file

---
+++ upload-terminologies.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+const { TITLE_TOKEN, PRICE_TOKEN, DESCRIPTION_TOKEN, MEAL_TITLE_TOKEN, ID_TOKEN, SEPERATOR } = require('./constants');
+
+const CSV = `en,de,fr
+${TITLE_TOKEN},${TITLE_TOKEN},${TITLE_TOKEN}
+${PRICE_TOKEN},${PRICE_TOKEN},${PRICE_TOKEN}
+${DESCRIPTION_TOKEN},${DESCRIPTION_TOKEN},${DESCRIPTION_TOKEN}
+${MEAL_TITLE_TOKEN},${MEAL_TITLE_TOKEN},${MEAL_TITLE_TOKEN}
+${ID_TOKEN},${ID_TOKEN},${ID_TOKEN}
+${SEPERATOR},${SEPERATOR},${SEPERATOR}`;
+
+const params = {
+	MergeStrategy: 'OVERWRITE',
+	Name: 'CUSTOM_TOKENS',
+	TerminologyData: {
+		File: Buffer.from(CSV),
+		Format: 'CSV',
+	},
+	Description: 'Custom Tokens to use with the menu data. These tokens should never be translated',
+};
+
+const translate = new AWS.Translate({ apiVersion: '2017-07-01', region: 'eu-central-1' });
+
+translate.importTerminology(params, function(err, data) {
+	if (err) {
+		console.log(err, err.stack);
+	} else {
+		console.log(data);
+	}
+});
